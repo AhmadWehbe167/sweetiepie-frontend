78b5a705ea14a4f98fa5c443d9308e87f15dd8df
ahmad wehbe <ahmdbwhbe@gmail.com>
2023-01-17 01:04:55 +0200

    header component

---
+++ src/components/header.jsx
@@ -0,0 +1,35 @@
+import "../assets/styles/header.css";
+import menu from "../assets/icons/menu.svg";
+import { useState } from "react";
+
+function Header() {
+  const [open, setOpen] = useState(false);
+
+  function handleOpen() {
+    setOpen(!open);
+  }
+
+  return (
+    <>
+      <img
+        src={menu}
+        alt="menu icon"
+        className="header__menu-icon"
+        onClick={handleOpen}
+      />
+      <div className="header-container">
+        <span className={"header__logo " + (open ? "header__logo--invisible" : "") + (open ? "" : "animate")}>
+          Sweetie Pies
+        </span>
+        <ul className={"header__navbar " + (open ? "" : "header__navbar--display ")}>
+          <li className="header__navlinks">Home</li>
+          <li className="header__navlinks">Shop</li>
+          <li className="header__navlinks">About</li>
+          <li className="header__navlinks">Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Header;
