2c6af10eef24dbd9cebb26defeea34aa3d38f30f
ahmad wehbe <ahmdbwhbe@gmail.com>
2023-02-01 16:08:18 +0200

    page not found simple UI

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,16 +1,14 @@
 // create a function component for not found page
 import React from "react";
-import { Link } from "react-router-dom";
+import bg from "../assets/images/home/location/background.webp";
 import "../assets/styles/pages/notFound.css";
 
 export default function NotFound() {
   return (
     <div className="not-found">
+      <img src={bg} alt="" className="not-found__img" />
       <h1 className="not-found__title">404</h1>
       <h2 className="not-found__subtitle">Page Not Found</h2>
-      <Link to="/" className="not-found__link">
-        Go Back
-      </Link>
     </div>
   );
 }
